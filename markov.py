590f832642e867df70772d98674b2739b3053471
Jademaster <35124881+Jademaster@users.noreply.github.com>
2020-12-05 16:52:50 -0800

    Add files via upload
    Created class for open transition matrices, functions for max-times multiplication, and pushout of open matrices.

---
+++ markov.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+class Openmat:
+    def __init__(self,array,inp,out):
+        self.mat=array
+        self.inp=inp
+        self.out=out
+
+def blackbox(array,inp, out):
+    print(len(out))
+    forget= np.zeros((len(inp),len(out)))
+    print(forget.shape)
+    for a in range(len(inp)):
+        for b in range(len(out)):
+            forget[a,b] = array[inp[a],out[b]]
+    return forget
+
+
+def mult(m1,m2):
+    product= np.zeros((m1.shape[0],m2.shape[1]))
+    for i in range(m1.shape[0]):
+        for j in range(m2.shape[1]):
+            product[i,j]= max([m1[i,k] * m2[k,j] for k in range(m1.shape[1])])
+    return product
+        
+def compose(openmat1,openmat2):
+    assert len(openmat1.out) == len(openmat2.inp), "open matrices have the wrong type and can't be composed"
+    size= 2*openmat1.mat.shape[0] - len(openmat1.out)
+    #extend the matrices to the pushout
+    extend1=np.zeros((size,size))
+    extend1[0:openmat1.mat.shape[0],0:openmat1.mat.shape[1]]=openmat1.mat
+    print(extend1)
+    extend2=np.zeros((size,size))
+    extend2[size-openmat2.mat.shape[0]:size,size-openmat2.mat.shape[1]:size]=openmat2.mat
+    print(extend2)
+    #compute the composite
+    composite=np.zeros((size,size))
+    composite=np.maximum(extend1,extend2)
+    return composite
+   
+a=np.random.rand(3,3)
+b=np.random.rand(3,3)
+
+in1= [0] 
+out1=[1]
+in2=[2]
+out2=[0,1]
+m1=Openmat(a,in1,out1)
+m2=Openmat(b,in2,out2)
+print(compose(m1,m2))
+
+
