e1dc9549742c4ad322c5976ff310d7a628d90a92
Jademaster <jmast003@ucr.edu>
2020-12-06 10:14:35 -0800

    added algorithm for most likely paths on a composite

--- markov.py
+++ markov.py
@@ -1,19 +1,11 @@
 import numpy as np
 
-class Openmat:
-
-    def __init__(self,array,inp,out):
-        self.mat=array
-        self.inp=inp
-        self.out=out
-
-    def blackbox(self):
-        forget= np.zeros((len(self.inp),len(self.out)))
-        for a in range(len(self.inp)):
-            for b in range(len(self.out)):
-                forget[a,b] = self.mat[self.inp[a],self.out[b]]
-        return forget
-
+def listmax(mats):
+    runningmax=np.zeros(mats[0].shape)
+    for m in mats:
+        runningmax=np.maximum(m,runningmax)
+    return runningmax
+    
 
 def mult(m1,m2):
     product= np.zeros((m1.shape[0],m2.shape[1]))
@@ -22,32 +14,72 @@ def mult(m1,m2):
             product[i,j]= max([m1[i,k] * m2[k,j] for k in range(m1.shape[1])])
     return product
         
+def power(m,n):
+    powers=[np.identity(m.shape[0])]
+    current=m
+    for i in range(n):
+        powers.append(current)
+        current= mult(m,current)
+    return powers
+
+
+class Openmat:
+
+    def __init__(self,array,inp,out):
+        self.mat=array
+        self.inp=inp
+        self.out=out
+        self.powers=[]
+        
+    def precompile(self,maxtime):
+        self.powers=power(self.mat,maxtime)
+
+    def blackbox(self,power=1):
+        forget= np.zeros((len(self.inp),len(self.out)))
+        for a in range(len(self.inp)):
+            for b in range(len(self.out)):
+                forget[a,b] = self.powers[power][self.inp[a],self.out[b]]
+        return forget
+ 
+
+
 def compose(openmat1,openmat2):
     assert len(openmat1.out) == len(openmat2.inp), "open matrices have the wrong type and can't be composed"
     size= 2*openmat1.mat.shape[0] - len(openmat1.out)
-    #extend the matrices to the pushout
+    #extend the matrices to the pushout of sets
     extend1=np.zeros((size,size))
     extend1[0:openmat1.mat.shape[0],0:openmat1.mat.shape[1]]=openmat1.mat
-    print(extend1)
     extend2=np.zeros((size,size))
     extend2[size-openmat2.mat.shape[0]:size,size-openmat2.mat.shape[1]:size]=openmat2.mat
-    print(extend2)
     #compute the composite
     composite=np.zeros((size,size))
     composite=np.maximum(extend1,extend2)
     return composite
 
-#compute the most likely path which occurs in n-steps
+def partitions(n):
+    if n==0:
+        return [(0,0)]
+    else:
+        partition=[(j[0]+1,j[1]) for j in partitions(n-1)]
+        partition.append((0,n))
+        return partition
+      
+def nmostlikely(m1,m2,n):
+    return listmax([mult(m1.blackbox(power=p[0]),m2.blackbox(power=p[1])) for p in partitions(n)])
+   
    
 a=np.random.rand(3,3)
 b=np.random.rand(3,3)
 
+
 in1= [0,1] 
 out1=[0,1]
-in2=[2]
+in2=[2,1]
 out2=[0,1]
 m1=Openmat(a,in1,out1)
 m2=Openmat(b,in2,out2)
-print(m1.blackbox())
+m1.precompile(5)
+m2.precompile(5)
+print(nmostlikely(m1,m2,5))
 
 
